6617bed21555e10a9610d3de5844ba0a86fb0d3f
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2023-04-13 12:01:24 +0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type = string
+  default = "us-east1"
+}
+
+variable "zone" {
+  type = string
+  default = "us-east1-c"
+}
+
+variable "project_id" {
+  type = string
+  default = "qwiklabs-gcp-02-15395569f17a"
+}
